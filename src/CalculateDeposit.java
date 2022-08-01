c3c6641b6cdef6bf7891cc426391a94bc0c73052
Enckey <kubenskye@ya.ru>
2022-08-01 13:32:46 +0300

    Issues fix

--- src/CalculateDeposit.java
+++ src/CalculateDeposit.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
 
-public class calculate_deposit {
-    double Calculate_Complex_Percent_Function(double a, int d) {
+public class CalculateDeposit {
+    double calculateComplexPercent(double a, int d) {
         double pay = a * Math.pow((1 + 0.06 / 12), 12 * d);
-        return rnd(pay);
+        return roundValue(pay);
     }
 
-    double Calculate_Simple_Percent_Function(double doubleAmount, int deposit_period) {
-        return rnd(doubleAmount + doubleAmount * 0.06 * deposit_period);
+    double calculateSimplePercent(double doubleAmount, int deposit_period) {
+        return roundValue(doubleAmount + doubleAmount * 0.06 * deposit_period);
     }
 
-    double rnd(double value) {
-        double ScaLe = Math.pow
-                (10, 2);
+    double roundValue(double value) {
+        double ScaLe = Math.pow(10, 2);
         return Math.round(value * ScaLe)
                 / ScaLe;
     }
 
-    void do_important_job() {
+    void calculatePercent() {
         int period, action;
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Введите сумму вклада в рублях:");
         int amount = scanner.nextInt();
         System.out.println("Введите срок вклада в годах:");
@@ -27,15 +27,16 @@ public class calculate_deposit {
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = scanner.nextInt();
         double outDoubleVar = 0;
-        if (action == 1) outDoubleVar = Calculate_Simple_Percent_Function(amount, period);
+        if (action == 1) outDoubleVar = calculateSimplePercent(amount, period);
         else if (action == 2) {
-            outDoubleVar = Calculate_Complex_Percent_Function(amount, period);
+            outDoubleVar = calculateComplexPercent(amount, period);
         }
         System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + outDoubleVar);
     }
 
-    public static void main(String[] args) {
-        new calculate_deposit().do_important_job();
+    public static void main(String[] args)
+    {
+        new CalculateDeposit().calculatePercent();
     }
 
 
