a58edec8e80bbc0eecaba6ae810520e9b8a8f21a
Enckey <kubenskye@ya.ru>
2022-08-01 13:44:43 +0300

    fix final

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-public class CalculateDeposit {
-    double calculateComplexPercent(double a, int d) {
-        double pay = a * Math.pow((1 + 0.06 / 12), 12 * d);
+public class DepositCalculator {
+    double calculateComplexPercent(double amount, int depositPeriod) {
+        double pay = amount * Math.pow((1 + 0.06 / 12), 12 * depositPeriod);
         return roundValue(pay);
     }
 
-    double calculateSimplePercent(double doubleAmount, int deposit_period) {
-        return roundValue(doubleAmount + doubleAmount * 0.06 * deposit_period);
+    double calculateSimplePercent(double amount, int depositPeriod) {
+        return roundValue(amount + amount * 0.06 * depositPeriod);
     }
 
     double roundValue(double value) {
@@ -26,17 +26,19 @@ public class CalculateDeposit {
         period = scanner.nextInt();
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = scanner.nextInt();
-        double outDoubleVar = 0;
-        if (action == 1) outDoubleVar = calculateSimplePercent(amount, period);
-        else if (action == 2) {
-            outDoubleVar = calculateComplexPercent(amount, period);
+        double percent = 0;
+        if (action == 1) {
+            percent = calculateSimplePercent(amount, period);
         }
-        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + outDoubleVar);
+        else if (action == 2) {
+           percent = calculateComplexPercent(amount, period);
+        }
+        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + percent);
     }
 
     public static void main(String[] args)
     {
-        new CalculateDeposit().calculatePercent();
+        new DepositCalculator().calculatePercent();
     }
 
 
